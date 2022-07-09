3b135ed114fc48eda85cdc16a779a4302855cbe8
phamtuyet <phamtuyet201201@gmail.com>
2022-07-09 15:34:24 +0700

    model crm_customer_request and view

---
+++ odoo/exam/crm_request_management/models/crm_customer_request.py
@@ -0,0 +1,19 @@
+from datetime import date
+from odoo import api, fields, models
+
+class CrmCustomerRequest(models.Model):
+    _name = "crm.customer.request"
+    _description = "request customer"
+    _rec_name = "id"
+
+    product_id = fields.Many2one("product.template",string= "Product",required = True)
+
+    opportunity_id = fields.Many2one("crm.lead", string="Opportunities", required = True ,domain="[('type', '=', 'opportunity')]")
+
+    date = fields.Datetime(string = "Date",default= date.today(), required = True)
+
+    quantity = fields.Float(string = "Quantity", required=True, default =1)
+
+
+
+
