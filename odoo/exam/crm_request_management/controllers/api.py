810fedcccc48bd4e3c968b2fa5ea956199b04674
phamtuyet <phamtuyet201201@gmail.com>
2022-07-11 08:37:41 +0700

    Exam: fix api

--- odoo/exam/crm_request_management/controllers/api.py
+++ odoo/exam/crm_request_management/controllers/api.py
@@ -11,22 +11,22 @@ class Api(http.Controller):
     @http.route('/api/insert_lead', auth='public', type='json')
     def insert_lead(self, **kwargs):
         if kwargs.get('token') == token:
-            # try:
-            vals = kwargs.get('request_ids')
-            request.env['crm.lead'].sudo().create({
-                'name': kwargs.get('name'),
-                'partner_id': kwargs.get('partner_id'),
-                'date_deadline': datetime.datetime.strptime(kwargs.get('date_deadline'), "%Y-%m-%d"),
-                'email_from': kwargs.get('email_from'),
-                'phone': kwargs.get('phone'),
-                'description': kwargs.get('description'),
-                'type': 'opportunity',
-                'request_ids': [(0, 0, val) for val in vals]
+            try:
+                vals = kwargs.get('request_ids')
+                request.env['crm.lead'].sudo().create({
+                    'name': kwargs.get('name'),
+                    'partner_id': kwargs.get('partner_id'),
+                    'date_deadline': datetime.datetime.strptime(kwargs.get('date_deadline'), "%Y-%m-%d"),
+                    'email_from': kwargs.get('email_from'),
+                    'phone': kwargs.get('phone'),
+                    'description': kwargs.get('description'),
+                    'type': 'opportunity',
+                    'request_ids': [(0, 0, val) for val in vals]
 
-            })
-        #     return 'done'
-        # except:
-        #      return 'false'
+                })
+                return 'done'
+            except:
+                 return 'false'
 
         else:
             return 'token invalid'
